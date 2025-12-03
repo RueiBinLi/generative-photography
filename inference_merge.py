209c64ef8a0a67d48ae95894b183dede2c5c3e39
RueiBinLi <s10959039@gm2.nutn.edu.tw>
2025-12-03 22:05:36 +0800

    merge the LoRA weights before load them in pipeline

--- inference_merge.py
+++ inference_merge.py
@@ -16,86 +16,34 @@ from genphoto.pipelines.pipeline_animation import GenPhotoPipeline
 from genphoto.models.unet import UNet3DConditionModelCameraCond
 from genphoto.models.camera_adaptor import CameraCameraEncoder, CameraAdaptor
 from genphoto.utils.util import save_videos_grid
+from inference_settings import Camera_Embedding_bokehK, Camera_Embedding_temp, Camera_Embedding_focal, Camera_Embedding_shutter
+
+import torch as nn
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def create_bokehK_embedding(bokehK_values, target_height, target_width):
-    f = bokehK_values.shape[0]
-    bokehK_embedding = torch.zeros((f, 3, target_height, target_width), dtype=bokehK_values.dtype)
-    
-    for i in range(f):
-        K_value = bokehK_values[i].item()
-        kernel_size = max(K_value, 1)
-        sigma = K_value / 3.0
+class EnsembleCameraEncoder(nn.Module):
+    def __init__(self, encoder_dict):
+        super().__init__()
+        self.encoders = nn.ModuleDict(encoder_dict)
 
-        ax = np.linspace(-(kernel_size / 2), kernel_size / 2, int(np.ceil(kernel_size)))
-        xx, yy = np.meshgrid(ax, ax)
-        kernel = np.exp(-(xx ** 2 + yy ** 2) / (2 * sigma ** 2))
-        kernel /= np.sum(kernel)
-        scale = kernel[int(np.ceil(kernel_size) / 2), int(np.ceil(kernel_size) / 2)]
-        
-        bokehK_embedding[i] = scale
-    
-    return bokehK_embedding
+    def forward(self, camera_embedding):
+        total_feature = None
 
-class Camera_Embedding(Dataset):
-    def __init__(self, bokehK_values, tokenizer, text_encoder, device, sample_size=[256, 384]):
-        self.bokehK_values = bokehK_values.to(device)
-        self.tokenizer = tokenizer
-        self.text_encoder = text_encoder
-        self.device = device  
-        self.sample_size = sample_size
+        for name, encoder in self.encoders.items():
+            if name in camera_embedding:
+                input = camera_embedding[name]
 
-    def load(self):
+                feature = encoder(input)
 
-        # Generate prompts for each bokehK value and append bokehK information to caption
-        prompts = []
-        for bb in self.bokehK_values:
-            prompt = f"<bokeh kernel size: {bb.item()}>"
-            prompts.append(prompt)
-        
-
-        # Tokenize prompts and encode to get embeddings
-        with torch.no_grad():
-            prompt_ids = self.tokenizer(
-                prompts, max_length=self.tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
-            ).input_ids.to(self.device)
-
-            encoder_hidden_states = self.text_encoder(input_ids=prompt_ids).last_hidden_state  # Shape: (f, sequence_length, hidden_size)
-        
-
-        # Calculate differences between consecutive embeddings (ignoring sequence_length)
-        differences = []
-        for i in range(1, encoder_hidden_states.size(0)):
-            diff = encoder_hidden_states[i] - encoder_hidden_states[i - 1]
-            diff = diff.unsqueeze(0)
-            differences.append(diff)  
-
-        # Add the difference between the last and the first embedding
-        final_diff = encoder_hidden_states[-1] - encoder_hidden_states[0]
-        final_diff = final_diff.unsqueeze(0)
-        differences.append(final_diff)
-
-        # Concatenate differences along the batch dimension (f-1)
-        concatenated_differences = torch.cat(differences, dim=0) 
-        frame = concatenated_differences.size(0)
-        # Concatenate differences along the batch dimension (f)
-        concatenated_differences = torch.cat(differences, dim=0)
-
-        pad_length = 128 - concatenated_differences.size(1)
-        if pad_length > 0:
-            concatenated_differences_padded = F.pad(concatenated_differences, (0, 0, 0, pad_length))
-
-
-        ccl_embedding = concatenated_differences_padded.reshape(frame, self.sample_size[0], self.sample_size[1])
-        ccl_embedding = ccl_embedding.unsqueeze(1)  
-        ccl_embedding = ccl_embedding.expand(-1, 3, -1, -1)
-        ccl_embedding = ccl_embedding.to(self.device)
-        bokehK_embedding = create_bokehK_embedding(self.bokehK_values, self.sample_size[0], self.sample_size[1]).to(self.device)
-        camera_embedding = torch.cat((bokehK_embedding, ccl_embedding), dim=1)
-        return camera_embedding
-
+                if total_feature is None:
+                    total_feature = feature
+                else:
+                    total_feature = total_feature + feature
+            else:
+                print(f"Warning: {name} embedding not found in input.")
+        return total_feature
 
 def load_models(cfg):
 
@@ -114,11 +62,17 @@ def load_models(cfg):
     ).to(device)
     unet.requires_grad_(False)
 
-    camera_encoder = CameraCameraEncoder(**cfg.camera_encoder_kwargs).to(device)
-    camera_encoder.requires_grad_(False)
-    camera_adaptor = CameraAdaptor(unet, camera_encoder)
-    camera_adaptor.requires_grad_(False)
-    camera_adaptor.to(device)
+    camera_encoder_bokehK = CameraCameraEncoder(**cfg.camera_encoder_kwargs).to(device)
+    camera_encoder_bokehK.requires_grad_(False)
+    camera_encoder_temp = CameraCameraEncoder(**cfg.camera_encoder_kwargs).to(device)
+    camera_encoder_temp.requires_grad_(False)
+    camera_encoder_focal = CameraCameraEncoder(**cfg.camera_encoder_kwargs).to(device)
+    camera_encoder_focal.requires_grad_(False)
+    camera_encoder_shutter = CameraCameraEncoder(**cfg.camera_encoder_kwargs).to(device)
+    camera_encoder_shutter.requires_grad_(False)
+    # camera_adaptor = CameraAdaptor(unet, camera_encoder)
+    # camera_adaptor.requires_grad_(False)
+    # camera_adaptor.to(device)
 
     logger.info("Setting the attention processors")
     unet.set_all_attn_processor(
@@ -144,21 +98,29 @@ def load_models(cfg):
         _, mm_u = unet.load_state_dict(mm_checkpoints, strict=False)
         assert len(mm_u) == 0
         print("Loading done")
-    
 
-    if cfg.camera_adaptor_ckpt is not None:
-        logger.info(f"Loading camera adaptor from {cfg.camera_adaptor_ckpt}")
-        camera_adaptor_checkpoint = torch.load(cfg.camera_adaptor_ckpt, map_location=device)
-        camera_encoder_state_dict = camera_adaptor_checkpoint['camera_encoder_state_dict']
-        attention_processor_state_dict = camera_adaptor_checkpoint['attention_processor_state_dict']
-        camera_enc_m, camera_enc_u = camera_adaptor.camera_encoder.load_state_dict(camera_encoder_state_dict, strict=False)
-        assert len(camera_enc_m) == 0 and len(camera_enc_u) == 0
-        _, attention_processor_u = camera_adaptor.unet.load_state_dict(attention_processor_state_dict, strict=False)
-        assert len(attention_processor_u) == 0
-        
-        logger.info("Camera Adaptor loading done")
-    else:
-        logger.info("No Camera Adaptor checkpoint used")
+    if cfg.camera_adaptor_ckpt_bokehK:
+        ckpt = torch.load(cfg.camera_adaptor_ckpt_bokehK, map_location=device)
+        camera_encoder_bokehK.load_state_dict(ckpt['camera_encoder_state_dict'], strict=False)
+
+    if cfg.camera_adaptor_ckpt_temp:
+        ckpt = torch.load(cfg.camera_adaptor_ckpt_temp, map_location=device)
+        camera_encoder_temp.load_state_dict(ckpt['camera_encoder_state_dict'], strict=False)
+
+    if cfg.camera_adaptor_ckpt_focal:
+        ckpt = torch.load(cfg.camera_adaptor_ckpt_focal, map_location=device)
+        camera_encoder_focal.load_state_dict(ckpt['camera_encoder_state_dict'], strict=False)
+
+    if cfg.camera_adaptor_ckpt_shutter:
+        ckpt = torch.load(cfg.camera_adaptor_ckpt_shutter, map_location=device)
+        camera_encoder_shutter.load_state_dict(ckpt['camera_encoder_state_dict'], strict=False)
+
+    ensemble_encoder = EnsembleCameraEncoder({
+        'bokehK': camera_encoder_bokehK,
+        'temp': camera_encoder_temp,
+        'shutter': camera_encoder_shutter,
+        'focal': camera_encoder_focal,
+    })
 
     pipeline = GenPhotoPipeline(
         vae=vae,
@@ -166,16 +128,10 @@ def load_models(cfg):
         tokenizer=tokenizer,
         unet=unet,
         scheduler=noise_scheduler,
-        camera_encoder=camera_encoder
+        camera_encoder=ensemble_encoder
     ).to(device)
-    pipeline.enable_vae_slicing()
 
-    if cfg.lora_ckpt is not None:
-        print(f"Loading the lora checkpoint from {cfg.lora_ckpt}")
-        
-        pipeline.load_lora_weights(cfg.lora_ckpt) 
-                
-        print(f'Loading done')
+    pipeline.enable_vae_slicing()
 
     return pipeline, device
 
@@ -187,14 +143,45 @@ def run_inference(pipeline, tokenizer, text_encoder, base_scene, bokehK_list, co
     bokehK_values = json.loads(bokehK_list_str)
     bokehK_values = torch.tensor(bokehK_values).unsqueeze(1)
 
-    # Ensure camera_embedding is on the correct device
-    camera_embedding = Camera_Embedding(bokehK_values, tokenizer, text_encoder, device).load()
-    camera_embedding = rearrange(camera_embedding.unsqueeze(0), "b f c h w -> b c f h w")
+    camera_embedding_bokehK = Camera_Embedding_bokehK(bokehK_values, tokenizer, text_encoder, device).load()
+    camera_embedding_bokehK = rearrange(camera_embedding_bokehK.unsqueeze(0), "b f c h w -> b c f h w")
+
+    color_temperature_list_str = color_temperature_list
+    color_temperature_values = json.loads(color_temperature_list_str)
+    color_temperature_values = torch.tensor(color_temperature_values).unsqueeze(1)
+    
+    num_frames = len(color_temperature_values)
+
+    camera_embedding_temp = Camera_Embedding_temp(color_temperature_values, tokenizer, text_encoder, device).load()
+    camera_embedding_temp = rearrange(camera_embedding_temp.unsqueeze(0), "b f c h w -> b c f h w")
+
+    focal_length_list_str = focal_length_list
+    focal_length_values = json.loads(focal_length_list_str)
+    focal_length_values = torch.tensor(focal_length_values).unsqueeze(1)
+
+    num_frames = len(focal_length_values)
+
+    camera_embedding_focal = Camera_Embedding_focal(focal_length_values, tokenizer, text_encoder, device).load()
+    camera_embedding_focal = rearrange(camera_embedding_focal.unsqueeze(0), "b f c h w -> b c f h w")
+
+    shutter_speed_list_str = shutter_speed_list
+    shutter_speed_values = json.loads(shutter_speed_list_str)
+    shutter_speed_values = torch.tensor(shutter_speed_values).unsqueeze(1)
+
+    camera_embedding_shutter = Camera_Embedding_shutter(shutter_speed_values, tokenizer, text_encoder, device).load()
+    camera_embedding_shutter = rearrange(camera_embedding_shutter.unsqueeze(0), "b f c h w -> b c f h w")
+
+    embeddings = {
+        'bokehK': camera_embedding_bokehK,
+        'temp': camera_embedding_temp,
+        'focal': camera_embedding_focal,
+        'shutter': camera_embedding_shutter,
+    }
 
     with torch.no_grad():
         sample = pipeline(
             prompt=base_scene,
-            camera_embedding=camera_embedding,
+            camera_embedding=embeddings,
             video_length=video_length,
             height=height,
             width=width,
